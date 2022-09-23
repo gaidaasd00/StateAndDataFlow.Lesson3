fc190c182c3e3f7870f87156bdb5897480bca533
gaidaasd00 <swiftgaida@list.ru>
2022-09-23 18:32:14 +0300

    Initial Commit

---
+++ StateAndDataFlow.Lesson3/StateAndDataFlow_Lesson3App.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlow_Lesson3App.swift
+//  StateAndDataFlow.Lesson3
+//
+//  Created by Алексей Гайдуков on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlow_Lesson3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
